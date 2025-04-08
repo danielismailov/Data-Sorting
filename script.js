f5d81fc931069b9c53f11dd9656634e66e0bc0e7
danielbisma05@gmail.com <danielbisma05@gmail.com>
2025-04-08 18:44:11 -0400

    added canvas

--- script.js
+++ script.js
@@ -47,3 +47,9 @@ function mergeSort(arr) {
     }
     return result.concat(left.slice(i)).concat(right.slice(j));
 }
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 150, 75);
+
